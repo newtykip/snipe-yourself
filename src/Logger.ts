754d28cbee67ffc26c2e1d96ec63ae7ba4d3c8c6
newtykins <newtykins@outlook.com>
2021-12-29 03:05:04 +0000

    feat(logger): add figures

--- src/Logger.ts
+++ src/Logger.ts
@@ -1,15 +1,16 @@
 import chalk from 'chalk';
+import figures from 'figures';
 
 export default class {
     static error(message: string) {
-        console.log(chalk.red(message));
+        console.log(chalk.red(`${chalk.bold(figures.cross)}  ${message}`));
     }
 
     static success(message: string) {
-        console.log(chalk.greenBright(message));
+        console.log(chalk.greenBright(`${chalk.bold(figures.tick)}  ${message}`));
     }
 
     static warn(message: string) {
-        console.log(chalk.yellow(message));
+        console.log(chalk.yellow(`${chalk.bold('?')}  ${message}`));
     }
 }
